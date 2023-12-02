4e1b85af31c26726c6c0e35d4193fbd29263da62
Bekario <adri2002.pardo@gmail.com>
2023-12-02 19:33:24 +0100

    Componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { BarraNavegacionComponent } from './barra-navegacion/barra-navegacion.component';
+import { BarraInferiorComponent } from './barra-inferior/barra-inferior.component';
+import { RegistroComponent } from './registro/registro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    BarraNavegacionComponent
+    BarraNavegacionComponent,
+    BarraInferiorComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
