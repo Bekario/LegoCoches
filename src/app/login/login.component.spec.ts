90ee69933d7363629b8583b545ffd0e99e2e375c
Antonio <antoniom220502@gmail.com>
2023-12-02 19:48:31 +0100

    metida barra

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 
+import { BarraInferior } from '../../components/barra-inferior/barra-inferior.component';
+
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
