45717c98af665aaee793cce1b5137e6b8293651c
Bekario <adri2002.pardo@gmail.com>
2023-12-02 12:56:24 +0100

    Subida Proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BarraNavegacionComponent } from './barra-navegacion/barra-navegacion.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    BarraNavegacionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
