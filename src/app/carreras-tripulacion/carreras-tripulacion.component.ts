33f5c2025281eddffd6a7cd06b73d21ae925631e
Bekario <adrian.pardoe@um.es>
2023-12-08 11:03:22 +0100

    Carreras bien hechas

--- src/app/carreras-tripulacion/carreras-tripulacion.component.ts
+++ src/app/carreras-tripulacion/carreras-tripulacion.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-carreras-tripulacion',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './carreras-tripulacion.component.css'
 })
 export class CarrerasTripulacionComponent {
+  constructor(private router: Router) { }
 
+  irAClasificacion(nombre: string) {
+    this.router.navigate(['clasificacion-campeonato',  { nombre }]);
+  }
 }
